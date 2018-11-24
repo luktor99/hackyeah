a93104139dbde94b937f6af2437c9cd89ba5d8fe
Piotr Sykulski <pitersk@gmail.com>
2018-11-24 14:50:08 +0100

    vibration and location start

---
+++ hackyeah_unity/Assets/LocationService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationService : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.location.Start ();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		// Access granted and location value could be retrieved
+		Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+	}
+
+
+}
---
+++ hackyeah_unity/Assets/Scripts/DirectionArrowController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionArrowController : MonoBehaviour {
+
+	float game_point_langtitude = 50.0f;
+	float game_point_longtitude = 50.0f;
+
+	float device_langtitude;
+	float device_longtitude;
+
+	long vibration_time_ms = 500;
+
+	AndroidJavaClass unity= new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	AndroidJavaObject ca = unity.GetStatic("currentActivity");
+	AndroidJavaClass vibratorClass = new AndroidJavaClass("android.os.Vibrator");
+	AndroidJavaObject vibratorService = ca.Call("getSystemService",ca.GetStatic("VIBRATOR_SERVICE"));
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vibratorService.Call("vibrate", 	vibration_time_ms);
+	}
+
+
+}
