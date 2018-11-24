370392200f3cfc2a24474c5066bbef2e443a770d
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-24 16:00:12 +0100

    Init page.

--- app/src/main/java/com/example/huber/myapplication/MainActivity.java
+++ app/src/main/java/com/example/huber/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);
+
 
         startLabel = (TextView)findViewById(R.id.startLabel);
         startButton = (Button) findViewById(R.id.startButton);
