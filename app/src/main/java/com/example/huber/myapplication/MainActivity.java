121c1d25026be02b9cb0323258ba6fb187ec84ad
HBuczynski <hubert.buczynski94@gmail.com>
2018-11-24 14:21:37 +0100

    Added Map Activity.

--- app/src/main/java/com/example/huber/myapplication/MainActivity.java
+++ app/src/main/java/com/example/huber/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.huber.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView startLabel;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLabel = (TextView)findViewById(R.id.startLabel);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
